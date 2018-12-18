5ddcf7812532ce5b1086befa6f2fdce40cdf2b89
ashamb <asha.m.b.110@gmail.com>
2018-12-18 16:42:28 +0530

    Export graph to pdf file done

--- src/app/components/hwc/hwc.component.ts
+++ src/app/components/hwc/hwc.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
+import * as jsPDF from 'jspdf'
 
 import { MatTableDataSource, MatPaginator } from "@angular/material";
 import { Ng4LoadingSpinnerService } from "ng4-loading-spinner";
 
 import { ConnectorService } from "../../services/connector.service";
 import { ExcelService } from "../../services/excel.service";
+
 import {
   IChartDataset,
   IHwcBlockA,
@@ -1288,7 +1290,7 @@ this.resultvillage.subscribe(res => {
             weight: 'bold'
           }
         }
-      }
+      },
     }
   });
 
@@ -1300,9 +1302,29 @@ this.resultvillage.subscribe(res => {
   });
   
   allVill.update();
-  });
+  
+});
+document.getElementById('download-pdf').addEventListener("click", downloadPDF);
+
+//donwload pdf from original canvas
+function downloadPDF() {
+  //var canvas = document.querySelector('#inVil');
+	//creates image
+  let canvas = <HTMLCanvasElement> document.getElementById('inVil');
+	var canvasImg = canvas.toDataURL("image/jpeg", 1.0);
+  
+	//creates PDF from img
+	var doc = new jsPDF('landscape');
+	doc.setFontSize(20);
+  doc.setFillColor(255, 255,255,0);
+  doc.rect(10, 10, 150, 160, "F");
+	doc.text(15, 15, "Cool Chart");
+	doc.addImage(canvasImg, 'JPEG', 10, 10, 280, 150 );
+	doc.save('canvas.pdf');
 }
 
+}
+ 
 //Top 30 ranges for all incidents
 
 private getrangeincidents(){
