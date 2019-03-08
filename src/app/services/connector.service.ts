3d92e23691bc724e50645b4fadc16f877d077cb0
ashamb <asha.m.b.110@gmail.com>
2019-03-08 17:01:24 +0530

    updated

--- src/app/services/connector.service.ts
+++ src/app/services/connector.service.ts
@@ -81,7 +81,6 @@ export class ConnectorService {
     return this.http.get(this.uri +'getImage/' + metaid);
 
 }
-
   
   getCompensationbyDate(fromDate, toDate): Observable<any> {
     return this.http.post<any>(this.uri + "getCompensation_ByDate", {
